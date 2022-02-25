420c1fdc09ecfa3ac52101219ab85c9477f27e7c
Johnni Mortensen <don@jo9.dk>
2022-02-25 11:11:21 +0100

    return json

--- api/hello.go
+++ api/hello.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+type Profile struct {
+	Name		string
+}
+
+
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world from Go!")
+	profile := Profile{"Johnni"}
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
